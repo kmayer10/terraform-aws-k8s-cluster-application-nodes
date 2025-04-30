ab51cb8c7b5247a8aeb21b3a3d834a859403c0c9
Kulbhushan <kulbhushan.mayer@gmail.com>
2025-04-30 10:43:06 +0530

    Added Output for Instance ID

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,6 @@ output "node_public_ip" {
 output "node_private_ip" {
     value = module.create_application_node.instance_private_ip
 }
+output "node_instance_id" {
+    value = module.create_application_node.instance_id
+}
